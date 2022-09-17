abfd584dc6d0c6a0e3aeb5d24add793e62d03735
Gabriel Arno <agafarno@gmail.com>
2022-09-17 09:44:15 +0200

    in class

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,19 @@
-alert("hello!");
+let paragraph = document.createElement("p");
+myElement.appendChild(paragraph);
+
+const createTodo = (text) => {
+    todos.push(text)
+}
 
 document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
+    e.preventDefault()
+    const text = e.target.elements.text.value.trim()
+
+
+    if (text.length > 0) {
+        createTodo(text)
+        e.target.elements.text.value = ''
+    }
+
+    console.log(todos)
 })
