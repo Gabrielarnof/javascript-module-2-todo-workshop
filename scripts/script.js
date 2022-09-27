28a3d9cbc60db66a7538e80b72bf139d7717d3a8
Gabriel Arno <agafarno@gmail.com>
2022-09-27 21:48:01 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,72 +1,81 @@
-let todos = []
+let todos = [];
 
-const removeTodo = (todoEl) => {
-    const todoIndex = todos.findIndex((todo) => {
-        return todo.toLowerCase() === todoEl.textContent.toLowerCase()
+const createTodo = (text) => {
+    todos.push({
+        title: text,
+        completed: false
     })
-    if (todoIndex > -1) {
-        todos.splice(todoIndex, 1)
+    saveTodosToLocalStorage();
+}
+
+const toggleTodo = (title) => {
+    const todo = todos.find((todo) => todo.title.toLowerCase() === title.toLowerCase())
+
+    if (todo) {
+        todo.completed = !todo.completed
+        saveTodosToLocalStorage();
     }
 }
+
+const generateTodoDOM = (todo) => {
+    const todoEl = document.createElement("label");
+    const containerEl = document.createElement("div");
+    const todoText = document.createElement("span");
+
+    // Setup the todo text
+    todoText.textContent = todo;
+    containerEl.appendChild(todoText);
+
+    // Setup container
+    todoEl.classList.add("list-item");
+    containerEl.classList.add("list-item__container");
+    todoEl.appendChild(containerEl);
+
+    // Setup the remove button
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "remove";
+    removeButton.classList.add("button", "button--text");
+    todoEl.appendChild(removeButton);
+    removeButton.addEventListener("click", () => {
+        removeTodo(todoText);
+        renderTodos(todos);
+    });
+
+    return todoEl;
+};
 
 const renderTodos = (todos) => {
-    const todoList = document.querySelector("#todos")
-    todoList.innerHTML = " "
+    const todoList = document.querySelector("#todos");
+    todoList.innerHTML = "";
 
     if (todos.length > 0) {
-        todos.forEach((todo) => { todoList.appendChild(generateTodoDOM(todo)) })
+        todos.forEach((todo) => {
+            todoList.appendChild(generateTodoDOM(todo));
+        });
     } else {
-        const messageEl = document.createElement('p')
-        messageEl.classList.add('empty-message')
-        messageEl.textContent = 'There are no todos to show'
-        todoList.appendChild(messageEl)
+        const messageEl = document.createElement("p");
+        messageEl.classList.add("empty-message");
+        messageEl.textContent = "There are no todos to show";
+        todoList.appendChild(messageEl);
     }
+};
 
+const removeTodo = (title) => {
+    const todoIndex = todos.findIndex((todo) => todo.title.toLowerCase() === title.toLowerCase())
+
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+        saveTodosToLocalStorage();
+    }
 }
-
-const generateTodoDOM = (todoObj) => {
-    const todoEl = document.createElement('label')
-    const containerEl = document.createElement('div')
-    const todoText = document.createElement('span')
-
-    // Setup todo checkbox
-    const checkbox = document.createElement('input')
-    checkbox.setAttribute('type', 'checkbox')
-    checkbox.checked = todoObj.completed
-    containerEl.appendChild(checkbox)
-    checkbox.addEventListener('change', () => {
-        toggleTodo(todoObj.title)
-        renderTodos(todos)
-    })
-
-    todoText.textContent = todoObj.title
-    containerEl.appendChild(todoText)
-
-    const removeButton = document.createElement('button')
-    removeButton.textContent = 'remove'
-    removeButton.classList.add('button', 'button--text')
-    todoEl.appendChild(removeButton)
-    removeButton.addEventListener('click', () => {
-        removeTodo(todoObj.title)
-        renderTodos(todos)
-    })
-
-    return todoEl
-}
-const createTodo = (text) => {
-    todos.push(text)
-}
-
-
-
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault()
-    const text = e.target.elements.text.value.trim()
-    console.log("texto añadido: " + text)
+document.querySelector("#new-todo").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const text = e.target.elements.text.value.trim();
 
     if (text.length > 0) {
-        createTodo(text)
-        renderTodos(todos)
+        createTodo(text);
+        e.target.elements.text.value = "";
     }
-    console.log(todos);
-})
+    renderTodos(todos);
+});
+renderTodos(todos);
